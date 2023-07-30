783ae1dfb29f0a408a0dbf06c03613aa436992a5
gurmanbenipal <134737006+gurmanbenipal@users.noreply.github.com>
2023-07-30 16:48:54 -0700

    add parts finished

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,11 @@
 
 from django.contrib import admin
 
-from .models import Pokemon
+from .models import Pokemon, Feeding, Toy
 # Register your models here.
 
 admin.site.register(Pokemon)
+admin.site.register(Feeding)
+admin.site.register(Toy)
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Pokemon
+from django.views.generic import ListView, DetailView
+from .models import Pokemon, Toy
+from .forms import FeedingForm
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -15,8 +17,12 @@ def pokemons_index(request):
 
 def pokemons_detail(request, pokemon_id):
   pokemon = Pokemon.objects.get(id=pokemon_id)
+  id_list = pokemon.toys.all().values_list('id')
+  toys_pokemon_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  feeding_form = FeedingForm()
   return render(request, 'pokemons/detail.html', {
-    'pokemon': pokemon
+    'pokemon': pokemon, 'feeding_form': feeding_form,
+    'toys': toys_pokemon_doesnt_have
   })
 
 class PokemonCreate(CreateView):
@@ -30,3 +36,39 @@ class PokemonUpdate(UpdateView):
 class PokemonDelete(DeleteView):
     model = Pokemon
     success_url = '/pokemons'
+    
+    
+def add_feeding(request, pokemon_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.pokemon_id = pokemon_id
+    new_feeding.save()
+  return redirect('detail', pokemon_id=pokemon_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+  
+  
+def assoc_toy(request, pokemon_id, toy_id):
+  Pokemon.objects.get(id=pokemon_id).toys.add(toy_id)
+  return redirect('detail', pokemon_id=pokemon_id)
+
+def unassoc_toy(request, pokemon_id, toy_id):
+  Pokemon.objects.get(id=pokemon_id).toys.remove(toy_id)
+  return redirect('detail', pokemon_id=pokemon_id)
